68340fea25e8d9ee17932b1b624f5c817fec11a7
Robert R Nelson <rnelson@monoxide.jpl.nasa.gov>
2026-05-19 15:11:26 -0700

    SIF CB

--- oco3_sam_success_rate.py
+++ oco3_sam_success_rate.py
@@ -35,7 +35,8 @@ else:
             active_title = "SAM Locations Colored by Success Rate (N converged CO2 retrievals > 200)"
         else:
             active_df = df_sif
-            active_colorscale = px.colors.sequential.Greens
+            #active_colorscale = px.colors.sequential.Greens
+            active_colorscale = px.colors.sequential.YlGl
             active_title = "SAM Locations Colored by Success Rate (N 'Best Quality' SIF retrievals > 200)"
 
         # Display the data for the currently selected dataset
